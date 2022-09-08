bedc5a4f98386a3500cf09978d5e65d3eae37335
Emanuel215B <113113747+Emanuel215B@users.noreply.github.com>
2022-09-08 11:15:16 -0500

    Add files via upload

---
+++ pub/testBotton.py
@@ -0,0 +1,85 @@
+
+#######################################################
+# Libraries
+########################################################
+
+import paho.mqtt.client as mqtt  # Library to MQTT Client
+from datetime import datetime    # Library to Extract Current Time
+import random                    # Library to get Random Numbers
+import time                      # Library of System Wait Time
+
+import RPi.GPIO as GPIO
+import time 
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(16,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+########################################################
+# Functions
+########################################################
+
+# Function to Connect MQTT Client to Broker
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+
+
+def on_publish(client,userdata,result):             #create function for callback
+    print(datetime.now().strftime("%H:%M:%S") +" - " +str(value) + " - data published \n")    
+    pass
+
+########################################################
+# Variables
+########################################################
+
+# Create Flags in class as Global Variables
+mqtt.Client.connected_flag = False 
+
+# MQTT Parameters
+broker="test.mosquitto.org"
+port = 1883         
+keepalive = 60      # Maximum time [Sec] with Broker Communication
+topic = "Test/Esp"
+
+########################################################
+# Initial Configuration
+########################################################
+
+# Create MQTT Instance
+client = mqtt.Client()
+
+# Configure MQTT Broker Connection Status Function             
+client.on_connect = on_connect       
+
+# Configure Receive Message Function
+client.on_publish = on_publish 
+
+########################################################
+# Main Code
+########################################################
+
+# Connect to MQTT Broker
+client.connect(broker, port, keepalive)
+
+
+# Action to realize
+while (True):
+     
+    input_state=GPIO.input(18)
+    if input_state==False:
+        status = client.publish(topic, ON)
+        time.sleep(0.2)
+
+    
+    
+    # Condition of Finnalization 
+    if(value>= 850):
+        break
+        
+    else:
+        time.sleep(10)
+        
+client.disconnect()
+print("End of Publish Data")
